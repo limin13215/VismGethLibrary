89526fdc44cea13f712ae3a1aeab85b366a9b0ed
Li min <limin@vism.com.cn>
2018-03-15 21:36:27 +0800

    获取gasLimit

--- vismgethlibrary/src/main/java/com/vism/gethlibrary/VismGeth.java
+++ vismgethlibrary/src/main/java/com/vism/gethlibrary/VismGeth.java
@@ -22,6 +22,8 @@ import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.utils.Numeric;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
@@ -43,9 +45,11 @@ public class VismGeth {
     public static final int HANDLER_MSG_WHAT = 0x1234;
     public static final String HANDLER_MSG_BALANCE = "TokenBalance";
     private Web3j web3;
+    private Credentials credentials = null;
+
     private Context context;
-    Credentials credentials = null;
     public Handler myHandler = null;
+    public String mKeystoreFilePath = null;
 
     public VismGeth(){}
     public VismGeth(Context context){
@@ -65,21 +69,23 @@ public class VismGeth {
      */
     public boolean createWallet(String password){
         try {
-            WalletUtils.generateLightNewWalletFile(password,context.getFilesDir());
+            String jsonName = WalletUtils.generateLightNewWalletFile(password,context.getFilesDir());
+            mKeystoreFilePath = jsonName;
+            Log.e(TAG,"keystore file`s name:  "+ jsonName);
         } catch (NoSuchAlgorithmException e) {
-            Log.e(TAG,"错误信息:"+e.getMessage());
+            Log.e(TAG,"createWallet() ERROR:"+e.getMessage());
             return false;
         } catch (NoSuchProviderException e) {
-            Log.e(TAG,"错误信息:"+e.getMessage());
+            Log.e(TAG,"createWallet() ERROR:"+e.getMessage());
             return false;
         } catch (InvalidAlgorithmParameterException e) {
-            Log.e(TAG,"错误信息:"+e.getMessage());
+            Log.e(TAG,"createWallet() ERROR:"+e.getMessage());
             return false;
         } catch (CipherException e) {
-            Log.e(TAG,"错误信息:"+e.getMessage());
+            Log.e(TAG,"createWallet() ERROR:"+e.getMessage());
             return false;
         } catch (IOException e) {
-            Log.e(TAG,"错误信息:"+e.getMessage());
+            Log.e(TAG,"createWallet() ERROR:"+e.getMessage());
             return false;
         }
         return true;
@@ -94,14 +100,57 @@ public class VismGeth {
      */
     public Credentials importWallet(String password, String keystore, String filename){
         try {
+            mKeystoreFilePath = filename;
             FileTools.savePrivateFiles(context,keystore,filename);
             credentials = WalletUtils.loadCredentials(password,context.getFilesDir()+"/"+filename);
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e(TAG,"importWallet(1,2,3) ERROR:"+e.getMessage());
+            return null;
         } catch (CipherException e) {
+            Log.e(TAG,"importWallet(1,2,3) ERROR:"+e.getMessage());
             return null;
         }
         return credentials;
+    }
+
+    /**
+     *   导入自己的钱包
+     * @param password
+     * @param filename
+     * @return
+     */
+    public Credentials importWallet(String password, String filename){
+        try {
+            mKeystoreFilePath = filename;
+            credentials = WalletUtils.loadCredentials(password,context.getFilesDir()+"/"+filename);
+        } catch (IOException e) {
+            Log.e(TAG,"importWallet(1,2) ERROR:"+e.getMessage());
+            return null;
+        } catch (CipherException e) {
+            Log.e(TAG,"importWallet(1,2) ERROR:"+e.getMessage());
+            return null;
+        }
+        return credentials;
+    }
+
+    public String getKeyStoreByName(){
+        String result = "";
+        try {
+            FileInputStream inputStream = new FileInputStream(context.getFilesDir()+"/"+mKeystoreFilePath);
+            byte temp[] = new byte[1024];
+            StringBuilder sb = new StringBuilder("");
+            int len = 0;
+            while ((len = inputStream.read(temp)) > 0){
+                sb.append(new String(temp, 0, len));
+            }
+            inputStream.close();
+            result = sb.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+
+        }
+        return result;
     }
 
     /**
@@ -109,7 +158,7 @@ public class VismGeth {
      *   通过Credentials的getAddress()方法可以获得钱包地址Address
      * @return Credentials
      */
-    public Credentials getCredentials(){
+    private Credentials getCredentials(){
         return credentials;
     }
 
@@ -213,7 +262,42 @@ public class VismGeth {
     }
 
     /**
-     *   估算代币gasLimit:
+     *    获取gasLimit
+     *
+     * @param contractAddress
+     * @return gasLimit
+     */
+    public String getGasLimit(String contractAddress){
+        String result = null;
+        String from = "";
+        if (credentials!=null){
+            from = credentials.getAddress();
+        }else {
+            from = "0x909Aa5ad1e3BB554347c763c06b8681E9C5A9694";
+        }
+        // get the next available nonce
+        try {
+            EthGetTransactionCount ethGetTransactionCount = web3.ethGetTransactionCount(from, DefaultBlockParameterName.LATEST).send();
+            BigInteger nonce = ethGetTransactionCount.getTransactionCount();
+            // get Contract function "transfer"
+            Function tranferFunc = ContractTools.transfer("0xe52307ebd18d20ab81f2abf582f1f0007ff658f0","1");
+            String encodedFunction = FunctionEncoder.encode(tranferFunc);
+            result = getEstimateGasLimit(from,
+                                        nonce,
+                                        new BigInteger("6000000000"),
+                                        new BigInteger(DEFAULT_GASLIMIT_TOKENS),
+                                        contractAddress,
+                                        encodedFunction
+                                        );
+        } catch (IOException e) {
+            //TODO
+            return DEFAULT_GASLIMIT_TOKENS;
+        }
+        return result;
+    }
+
+    /**
+     *   封装以太坊估算gasLimit方法:
      *        计算代币进行合约转账时，消耗的gas数量
      * @param address
      * @param nonce
@@ -223,7 +307,7 @@ public class VismGeth {
      * @param encodedFunction
      * @return String
      */
-    public String getEstimateGasLimit(String address, BigInteger nonce,
+    private String getEstimateGasLimit(String address, BigInteger nonce,
                                       BigInteger gasPrice, BigInteger gasLimit,
                                       String contractAddress, String encodedFunction) {
         String result ="";
@@ -276,7 +360,7 @@ public class VismGeth {
                     @Override
                     public void onRequestComplete(String result) {
                         String balance = JsonTools.parseJSONWithJSONObject(result);
-                        Log.e(TAG,"resultJson解析111: balance="+balance);
+                        //Log.e(TAG,"resultJson解析111: balance="+balance);
                         Message msg = myHandler.obtainMessage();
                         msg.what = HANDLER_MSG_WHAT;
                         Bundle bundle = new Bundle();
